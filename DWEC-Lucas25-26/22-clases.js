88030b111d127cb46fe5082c00e29587b146b433
LucasitOP <lucas.timoc1@gmail.com>
2025-10-29 12:26:28 +0100

    Create 22-clases.js

---
+++ DWEC-Lucas25-26/22-clases.js
@@ -0,0 +1,15 @@
+// Clases
+
+class Persona{
+
+    constructor(nombre, edad, email){
+        this.nombre = nombre;
+        this.edad = edad;
+        this.email = email;
+
+    }
+    constructor(){
+        
+    }
+
+}
